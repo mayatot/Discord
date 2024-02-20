62f3e30b57eb18ee8095b9a32b8f5a94a49811e3
mayatot <160620707+mayatot@users.noreply.github.com>
2024-02-21 00:45:27 +0800

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ client.on('ready', async () => {
   console.log(`${client.user.tag} - rich presence started!`);
 
   const r = new Discord.RichPresence()
-    .setApplicationId('')
+    .setApplicationId('1201106199383375942')
     .setType('STREAMING')
     .setURL('https://www.youtube.com/watch?v=dQw4w9WgXcQ') //Must be a youtube video link 
     .setState('Valorant')
@@ -44,7 +44,7 @@ client.on('ready', async () => {
   setInterval(() => {
     const newTime = formatTime();
     if (newTime !== prevTime) {
-      const newDetails = ` .gg/eporium`;
+      const newDetails = `kapoi`;
       r.setDetails(newDetails);
       client.user.setActivity(r);
       prevTime = newTime;
